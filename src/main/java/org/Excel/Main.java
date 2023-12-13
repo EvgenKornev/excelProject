4ebe52dc50b80be6489354d8eb6739442625aaae
Evgen Kornev <evgenkorneu@gmail.com>
2023-12-13 07:47:16 +0300

    project is work

--- src/main/java/org/Excel/Main.java
+++ src/main/java/org/Excel/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //GraphicInterface.run(); // Запуск графического интерфейса
 
-        new StartMenu().setVisible(true);
+        new StartPage().setVisible(true);
 
     }
 }
--- src/main/java/org/Excel/ProgramMenu.java
+++ src/main/java/org/Excel/ProgramMenu.java
@@ -13,7 +13,7 @@ public class ProgramMenu extends JFrame {
 
     public ProgramMenu() {
         this.setTitle("О программе");
-        this.setSize(400, 400);
+        this.setSize(400, 200);
         this.setLocationRelativeTo(null);
         //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
@@ -30,11 +30,24 @@ public class ProgramMenu extends JFrame {
         textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
 
 
-        JLabel programName = new JLabel("Составление списка студентов из абитуриентов");
+
+        JLabel programName = new JLabel("Программа позволяет:");
+        JLabel label1 = new JLabel("1. Получать файл расширения .xls");
+        JLabel label2 = new JLabel("2. Указывать путь получения файла");
+        JLabel label3 = new JLabel("3. Отображать данные файла в программе");
+        JLabel label4 = new JLabel("4. Рассчитывать студентов по заданным параметрам");
+        JLabel label5 = new JLabel("5. Указывать путь сохранения файла");
+        JLabel label6 = new JLabel("6. Сохранять файл с расширением .xls");
 
         authorButton.setAlignmentX(CENTER_ALIGNMENT);
 
         textPanel.add(programName);
+        textPanel.add(label1);
+        textPanel.add(label2);
+        textPanel.add(label3);
+        textPanel.add(label4);
+        textPanel.add(label5);
+        textPanel.add(label6);
         buttonPanel.add(authorButton);
 
         mainPanel.add(textPanel);
@@ -52,12 +65,4 @@ public class ProgramMenu extends JFrame {
         });
     }
 
-    // Метод для открытия ссылки в браузере
-    private static void openWebpage(String url) {
-        try {
-            Desktop.getDesktop().browse(new URI(url));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
